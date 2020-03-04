171d48fcfef544f9fee41f409601241fbadeef21
Ephrem Demelash <demelashephrem@gmail.com>
2020-03-04 10:11:06 +0300

    rating added

---
+++ migrations/20200302170213-create-rate.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Rates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      productId: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Rates');
+  }
+};

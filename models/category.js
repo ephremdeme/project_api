064dea85e2b7e8a11a56af55d84919fca75b1784
Ephrem Demelash <demelashephrem@gmail.com>
2020-03-10 16:08:54 +0300

    add category

---
+++ models/category.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    category: DataTypes.STRING
+  }, {});
+  Category.associate = function(models) {
+    // associations can be defined here
+    Category.hasMany(models.Product);
+  };
+  return Category;
+};

0e47ee534a48332301646d1d0a27b30bc63417bc
Ephrem Demelash <demelashephrem@gmail.com>
2020-12-25 20:10:19 +0300

    📦 NEW: adds permission for accesss control

--- src/middleware/permissions.js
+++ src/middleware/permissions.js
@@ -1,13 +1,44 @@
-const { rule, shield } = require("graphql-shield");
+const { rule, shield, deny, allow, or } = require("graphql-shield");
 
 const isAuthenticated = rule()((parent, args, { user }) => {
   return user !== null;
+});
+
+
+const isAdmin = rule()( async (parent, args, { user, models }) => {
+  admin = await models.User.findOne({
+    where: {
+      id: user.id,
+    },
+    include: models.Role,
+  });
+
+  return true
+});
+
+
+
+const isCommentOwner = rule()(async (parent, args, { models, user }) => {
+  let comment = await models.Comment.findOne({
+    where : {
+      UserId : user.id
+    }
+  })
+  return comment!=null;
 });
 
 const permissions = shield({
   Query: {
     getUser: isAuthenticated,
     users: isAuthenticated,
+  },
+
+  Mutation: {
+    "*": isAuthenticated,
+    deleteComment : or(isCommentOwner, isAdmin),
+    updateComment : isCommentOwner,
+    createUser: allow,
+    login: allow,
   },
 });
 

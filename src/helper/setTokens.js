2b3eabf70bd884759c87f6ca2e243f016eb3b4af
Ephrem Demelash <demelashephrem@gmail.com>
2020-12-25 23:11:58 +0300

    adds roles to the payload

--- src/helper/setTokens.js
+++ src/helper/setTokens.js
@@ -1,17 +1,21 @@
 import { sign } from "jsonwebtoken";
 
-
-export function setTokens(user) {
+export async function setTokens(user) {
   const sevenDays = 60 * 60 * 24 * 7 * 1000;
   const oneDay = 24 * 60 * 60 * 1000;
+  let data = await user.getRoles();
+  let roles = data.map((role) => role.toJSON().role);
   const accessUser = {
     id: user.id,
+    roles: roles,
   };
+
   const accessToken = sign(
     { user: accessUser },
-    Buffer.from(process.env.ACESS_TOKEN_SECRET_KEY).toString('base64'),
+    Buffer.from(process.env.ACESS_TOKEN_SECRET_KEY).toString("base64"),
     {
-      expiresIn: oneDay,
+      expiresIn: sevenDays,
+      algorithm: "HS256",
     }
   );
   const refreshUser = {
@@ -20,8 +24,9 @@ export function setTokens(user) {
   };
   const refreshToken = sign(
     { user: refreshUser },
-    Buffer.from(process.env.REFRESH_TOKEN_SECRET_KEY).toString('base64'),
+    Buffer.from(process.env.REFRESH_TOKEN_SECRET_KEY).toString("base64"),
     {
+      algorithm: "HS256",
       expiresIn: sevenDays,
     }
   );

0613562a9ca610894756a5743b6861d6c7d912d1
Ephrem Demelash <demelashephrem@gmail.com>
2020-12-28 19:51:12 +0300

    adds id field to table

--- src/models/wishlist.js
+++ src/models/wishlist.js
@@ -1,11 +1,21 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Wishlist = sequelize.define('Wishlist', {
-  }, {});
-  Wishlist.associate = function(models) {
+  const Wishlist = sequelize.define(
+    "Wishlist",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {}
+  );
+  Wishlist.associate = function (models) {
     // associations can be defined here
-    Wishlist.belongsTo(models.User)
-    Wishlist.belongsTo(models.Product)
+    Wishlist.belongsTo(models.User);
+    Wishlist.belongsTo(models.Product);
   };
   return Wishlist;
 };

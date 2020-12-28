b29c284a399208fd10654a9db6be36aec938ccde
Ephrem Demelash <demelashephrem@gmail.com>
2020-12-28 19:50:54 +0300

    📦 NEW: user roles n:m table

---
+++ src/models/userRole.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const UserRoles = sequelize.define('UserRoles', {
+  }, {});
+  UserRoles.associate = function(models) {
+    // associations can be defined here
+  };
+  return UserRoles;
+};
